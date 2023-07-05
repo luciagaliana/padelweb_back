23a12f34662fa77266f1fc43e6d3bf583342aec9
luciagaliana <luciagaliana8@gmail.com>
2023-07-05 12:02:20 +0200

    2023_07_05 Ruta para incio de sesión, ruta para autenticar usuario y contraseña cifrada

--- src/controllers/users.controllers.js
+++ src/controllers/users.controllers.js
@@ -1,5 +1,52 @@
 import {pool} from '../db.js'
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
 
+
+export const loginUser = async (req, res) => {
+  try {
+    const { correo, contrasenya } = req.body;
+
+    // Verificar si el usuario existe
+    const [existingUsers] = await pool.query(
+      'SELECT * FROM users WHERE correo = ?',
+      [correo]
+    );
+
+    if (existingUsers.length == 0 ) {
+      // Usuario no encontrado, enviar mensaje de error
+      return res.status(400).send('No existe un usuario con ese correo.');
+    }
+
+    const user = existingUsers[0];
+    const userId = user.id;
+
+    // Verificar la contraseña
+    const passwordMatch = await bcrypt.compare(contrasenya, user.contrasenya);
+
+    if (!passwordMatch) {
+      // Contraseña incorrecta, enviar mensaje de error
+      return res.status(400).send('Contraseña incorrecta.');
+    }
+
+    const userIdForToken = {
+      id: userId
+    }
+
+    // Generar token JWT
+    const token = jwt.sign(userIdForToken, 'secretKey', {expiresIn: '1d'});
+    res.cookie('token', token)
+
+    // Usuario y contraseña correctos
+    res.status(200).send('El incio de sesión se ha realizado correctamente.');
+
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: 'Algo ha ido mal.'
+    });
+  }
+};
 
 export const getUsers  =  async (req,res)=>{
     const [rows] = await pool.query(
@@ -29,17 +76,20 @@ export const createUser = async (req, res) => {
     const [existingUsers] = await pool.query(
       'SELECT * FROM users WHERE correo = ?', [correo]
     );
+    const hashedPassword = await bcrypt.hash(contrasenya, 10);
 
     if (existingUsers.length > 0) {
       // Usuario existente, enviar mensaje de error
       return res.status(400).send("Ya existe un usuario con ese correo.");
     }
 
+    //Si todo ha ido bien insertamos el nuevo usuario en la tabla users de la bd
     const [rows] = await pool.query(
       'INSERT INTO users (id, nombre, apellido1, apellido2, correo, contrasenya, genero, propietario, edificio, escalera, piso, letra, admin) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?)',
-      [id, nombre, apellido1, apellido2, correo, contrasenya, genero, propietario, edificio, escalera, piso, letra, admin]
+      [id, nombre, apellido1, apellido2, correo, hashedPassword, genero, propietario, edificio, escalera, piso, letra, admin]
     );
     
+    //QUITAR AL TERMINAR TRABAJO
     console.log(req.body);
     res.send("Se ha registrado el nuevo usuario");
 
@@ -49,8 +99,6 @@ export const createUser = async (req, res) => {
     })
   }
 };
-
-
 
 export const editUser = (req,res)=>res.send('actualizando usuarios');
 
--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -1,7 +1,42 @@
 import {Router} from 'express'
-import { getUsers , createUser, editUser, deleteUser} from '../controllers/users.controllers.js'
+import { 
+    getUsers, 
+    createUser, 
+    editUser, 
+    deleteUser, 
+    loginUser, 
+} from '../controllers/users.controllers.js'
+import jwt from 'jsonwebtoken';
 
 const router = Router()
+
+//Ruta para el inicio de sesión
+router.post('/login', loginUser); 
+
+//Ruta para autenticar al usuario
+router.get('/auth', async (req, res, next) => {
+    const authorization = req.get('authorization')
+
+    let token = null;
+
+    if (authorization && authorization.toLowerCase().startsWith('bearer')){
+        token = authorization.substring(7)
+    }
+
+    let decodedToken = null
+    try{
+        decodedToken = jwt.verify(token, "secretKey")
+    } catch {}
+
+    if (!token || !decodedToken){
+        return res.status(401).json({error: "token is missing or invalid"});
+    }
+
+    return res.status(200).json(decodedToken)
+});
+
+//Ruta para deslogear al usuario
+//router.get('/logout', logOutUser);
 
 router.get('/users', getUsers);
 
