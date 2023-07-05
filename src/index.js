1dae2a664fed58ad3bd02566096f009ccbd00000
luciagaliana <luciagaliana8@gmail.com>
2023-07-05 11:59:48 +0200

    2023_07_05 Agregación dependencias cookie-parser, bcrypt, jsonwebtoken

--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@
 import { PORT } from './config.js'
 import express from 'express'
 import cors from 'cors';
-
+import cookieParser from  'cookie-parser';
 import usersRoutes from './routes/users.routes.js'
 
 const app = express();
 
+const allowedOrigins = ['*', 'http://localhost:3000'];
+
 app.use(cors({
-    origin: '*',
+    origin: 'http://localhost:3000',
     methods: 'GET,PUT,POST,DELETE',
+    credentials: true
 }));
 
 app.use(express.json())
+app.use(cookieParser())
 
 app.use(usersRoutes);
 
